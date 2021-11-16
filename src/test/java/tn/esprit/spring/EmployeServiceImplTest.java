e19f3097213e028936d030efa34bcb723444645e
jihen1 <60669339+jihen1@users.noreply.github.com>
2021-11-16 12:50:54 +0100

    Update EmployeServiceImplTest.java

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -36,7 +36,7 @@ public class EmployeServiceImplTest {
 	public void testGetEmployePrenomById() {
 		try {
 			int idE = employeS
-					.ajouterEmploye(new Employe("jihen", "barbouche", "barbouchejihen1@gmail.com", true, Role.TECHNICIEN));
+					.ajouterEmploye(new Employe("jihen", "jihen", "barbouchejihen1@gmail.com", true, Role.TECHNICIEN));
 			String prenomEmp = employeS.getEmployePrenomById(idE);
 			l.info("------> Prenom de lemploye est : " + prenomEmp);
 			assertThat(prenomEmp).isEqualTo("jihen");
@@ -52,7 +52,7 @@ public class EmployeServiceImplTest {
 	public void testAjouterEmploye() {
 		try {
 			int id = employeS
-					.ajouterEmploye(new Employe("Mehdy", "benromdhane", "mehdy.benromdhane@spring.tn", true, Role.INGENIEUR));
+					.ajouterEmploye(new Employe("jihen", "barbouche", "barbouchejihen1@gmail.com", true, Role.INGENIEUR));
 
 			assertThat(id).isPositive();
 			l.info("Employe added successfully!");
@@ -66,9 +66,9 @@ public class EmployeServiceImplTest {
 	@Test
 	public void testMettreAjourEmailByEmployeId() {
 		try {
-			String email = "mehdy.benromdhane@spring.tn";
+			String email = "barbouchejihen1@gmail.com";
 			int id = employeS
-					.ajouterEmploye(new Employe("Mehdy", "benromdhane", "mehdy.benromdhane@gmail.com", true, Role.INGENIEUR));
+					.ajouterEmploye(new Employe("jihen", "barbouche", "barbouchejihen1@gmail.com", true, Role.INGENIEUR));
 
 			employeS.mettreAjourEmailByEmployeId(email, id);
 
@@ -80,37 +80,13 @@ public class EmployeServiceImplTest {
 			l.error(String.format("Erreur dans Mettre A jour Email By Employe Id : %s ", e));
 		}
 	}
-/*
-	@Test
-	public void affecterEmployeADepartement() {
-		try {
-			int idE = employeS
-					.ajouterEmploye(new Employe("Mehdy", "benromdhane", "mehdy.benromdhane@gmail.com", true, Role.TECHNICIEN));
-			int idD = entrepriseS.ajouterDepartement(new Departement("info"));
 
-			employeS.affecterEmployeADepartement(idE, idD);
-			l.info("--->Employe added successfully to Departement ");
-			l.info("---->desaffectaion, de lemployer de departement");
-			employeS.desaffecterEmployeDuDepartement(idE, idD);
-			employeS.deleteEmployeById(idE);
-			assertThat(idE).isNull();
-
-			entrepriseS.deleteDepartementById(idD);
-			assertThat(idD).isNull();
-
-
-		} catch (Exception e) {
-			l.error(String.format("Erreur dans l'affectaion : %s ", e));
-		}
-
-	}
-*/
 	@Test
 	public void testDeleteEmployeById() {
 
 		try {
 			int id = employeS
-					.ajouterEmploye(new Employe("Mehdy", "benromdhane", "mehdy.benromdhane@gmail.com", true, Role.INGENIEUR));
+					.ajouterEmploye(new Employe("jihen", "barbouche", "barbouchejihen1@esprit.tn", true, Role.INGENIEUR));
 
 			employeS.deleteEmployeById(id);
 
