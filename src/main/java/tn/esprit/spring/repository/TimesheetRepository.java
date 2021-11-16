5fc31d195cb77e066db82aa5d4a9994b59c0fd83
Ahmed Aziz Smaoui <60669339+jihen1@users.noreply.github.com>
2021-11-16 11:17:52 +0100

    fixbugs

---
+++ src/main/java/tn/esprit/spring/repository/TimesheetRepository.java
@@ -0,0 +1,20 @@
+package tn.esprit.spring.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.TimesheetPK;
+
+@Repository
+public interface TimesheetRepository extends CrudRepository<Timesheet, Integer> {
+
+	
+}
